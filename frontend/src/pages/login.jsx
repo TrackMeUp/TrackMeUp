bb8cb3b75a43df4a0533fc66858f6f69917cb186
Elena <elenamzuma@gmail.com>
2025-03-25 13:41:41 +0100

    Login CSS
    Estilo de la página de formulario de acceso

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"; // Para usar funcionalidades de React en versiones anteriores
 import { UserController } from "../controllers/UserController";
-//import './login.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../styles/login.css';
 
 export function Login() {
     // Validación del formulario (Cliente)
@@ -19,35 +20,43 @@ export function Login() {
     return (
         <section>
             <form id="loginForm" action="login" onSubmit={handleLogin}>
-                <div>
-                    <label htmlFor="user">Usuario: </label>
-                    <input
-                        type="text"
-                        id="user"
-                        name="user"
-                        value={user}
-                        onChange={(e) => setUser(e.target.value)}
-                    />
-                </div>
+                <div className="login-containter">
+                    <div className="login-card">
+                        <h3 className="login-title">Acceso</h3>
+                        <div className="login-group">
+                            <label htmlFor="user" className="form-label">Usuario: </label> <br/>
+                            <input
+                                type="text"
+                                className="form-data"
+                                id="user"
+                                name="user"
+                                value={user}
+                                onChange={(e) => setUser(e.target.value)}
+                            />
+                           
+                        </div>
 
-                <div>
-                    <label htmlFor="password">Contraseña: </label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
+                        <div className="login-group">
+                            <label htmlFor="password" className="form-label">Contraseña: </label> <br/>
+                            <input
+                                type="password"
+                                className="form-data"
+                                id="password"
+                                name="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
 
-                <div>
-                    <button type="submit">Iniciar sesión</button>
-                </div>
+                        <div>
+                            <button type="submit" className="login-button">Iniciar sesión</button>
+                        </div>
 
-                <div>
-                    <a href="">¿Has olvidado tu contraseña?</a>
-                    {/* Completar ruta */}
+                        <div>
+                            <a href="" className = "passw-button">¿Has olvidado tu contraseña?</a>
+                            {/* Completar ruta */}
+                        </div>
+                    </div>
                 </div>
             </form>
         </section>
