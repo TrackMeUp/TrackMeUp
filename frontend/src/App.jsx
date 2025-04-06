e1742838ecbcf3579e20b77a1f92a02a07af9ba9
Elena <elenamzuma@gmail.com>
2025-04-06 19:21:07 +0200

    Navegación y login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,47 +16,32 @@ import { Announcements } from './pages/announcements';
 import { Notifications } from './pages/notice_board';
 import { Login } from './pages/login';
 import { AdminView } from "./pages/admin/adminView";
+import { LogOut } from './pages/logout';
 
 
 export function App() {
 
-  const isLogged = true; // Simulación de inicio de sesión
-
   return (
+    
     <>
       <Routes>
-
-        {isLogged ? (
-
           // Si el usuario ha iniciado sesión, se muestra el menú de navegación (Layout)
 
+          <Route path="/" element={localStorage.getItem("user") ? <Navigate to="/home" /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
           <Route element={<Layout />}>
-
-            {/* <Route path="/" element={<Navigate to="/login" />} /> */}
-
-            <Route path="/" element={<Navigate to="/home" />} />
-
-            <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/academic_info" element={<AcademicInfo />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/activities" element={<Activities />} />
-            <Route path="/performance" element={<Performance />} />
-            <Route path="/communication" element={<Communication />} />
-            <Route path="/announcements" element={<Announcements />} />
-            <Route path="/notice_board" element={<Notifications />} />
-            <Route path="/admin" element={<AdminView />} />
-
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/academic_info" element={<AcademicInfo />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/activities" element={<Activities />} />
+          <Route path="/performance" element={<Performance />} />
+          <Route path="/communication" element={<Communication />} />
+          <Route path="/announcements" element={<Announcements />} />
+          <Route path="/notice_board" element={<Notifications />} />
+          <Route path="/admin" element={<AdminView />} />
+          <Route path="/logout" element={<LogOut />} />
           </Route>
-
-        ) : (
-
-          // Si no ha iniciado sesión, mostramos el login
-
-          <Route path="*" element={<Login />} />
-        )}
-        
       </Routes>
 
 
