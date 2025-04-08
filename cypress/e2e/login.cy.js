1c118c3563933011365c8b14b059883ca2e00980
Elena <elenamzuma@gmail.com>
2025-04-08 20:36:38 +0200

    Prueba E2E login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,4 @@
+// Prueba E2E para iniciar sesión
 describe('Prueba de inicio de sesión', () => {
 
   beforeEach(() => {
@@ -5,22 +6,69 @@ describe('Prueba de inicio de sesión', () => {
   })
 
   it('Se permite al usuario iniciar sesión introduciendo credenciales válidas', () => {
-    cy.get('#user').type('usuario_prueba'); // Completar campo usuario
+    cy.get('#user').type('usuario@prueba.com'); // Completar campo usuario
     cy.get('#password').type('contraseña'); // Completar campo contraseña
     cy.get('.login-button').click(); // Pulsar botón iniciar sesión
 
-    cy.url().should('include', '/'); // Verificar que accede correctamente a la página home
+    // cy.url().should('include', '/'); // Verificar que accede correctamente a la página home
+    
+    cy.url().should('include', 'http://localhost:5173/home'); // Verificar que accede correctamente a la página home
 
-    //cy.contains('').should('be.visible'); // Verificar que se muestra contenido después de iniciar sesión
-
+    // cy.contains('').should('be.visible'); // Verificar que se muestra contenido después de iniciar sesión
 
   })
 
-  it('Se muestra un mensaje de error si las credenciales no son válidas'), () => {
-  cy.get('#user').type('usuarioIncorrecto');
-  cy.get('#password').type('contraseñaIncorrecta');
-  cy.get('.login-button').click(); 
+  it('Se muestra un mensaje de error si no se completan todos los campos', () => {
+    cy.visit('http://localhost:5173/login') // Visitar la página de login
 
-  //cy.contains('').should('be.visible'); // Verificar que se muestra un mensaje de error
-  }
+    //cy.get('#user').type('usuario@prueba.com');
+    //cy.get('#password').type('');
+    cy.get('.login-button').click();
+
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('Debe rellenar todos los campos para iniciar sesión.'); // Verificar que se muestra un mensaje de error
+    })
+  })
+
+  it('Se muestra un mensaje de error si el usuario introducido no es válido', () => {
+    cy.visit('http://localhost:5173/login') // Visitar la página de login
+
+    cy.get('#user').type('usuarioIncorrecto');
+    cy.get('#password').type('contraseña');
+    cy.get('.login-button').click();
+
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('El usuario debe ser un correo electrónico.'); // Verificar que se muestra un mensaje de error
+    })
+  })
+
+  it('Se muestra un mensaje de error si la contraseña introducida no es válida', () => {
+    cy.visit('http://localhost:5173/login') // Visitar la página de login
+
+    cy.get('#user').type('usuario@prueba.com');
+    cy.get('#password').type('passw');
+    cy.get('.login-button').click();
+
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal('La contraseña debe tener al menos 6 caracteres.'); // Verificar que se muestra un mensaje de error
+    })
+  })
+  
+  it('Se muestra un mensaje informativo al solicitar un cambio de contraseña', () => {
+    cy.visit('http://localhost:5173/login') // Visitar la página de login
+    cy.get('.passw-button').click();
+
+    cy.on('window:alert', (text) => {
+      
+      expect(text).to.equal('El centro de estudios se pondrá en contacto contigo próximamente para proporcionarte una nueva contraseña.'); 
+    })
+  })
+
+
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    // Previene que Cypress falle el test con excepciones no controladas
+    return false;
+  })
+  
+  
 })
