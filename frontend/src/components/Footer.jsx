a46e6dd11b7e4a14d8d7fd7b2e51cf240bfc7f9a
Elena <elenamzuma@gmail.com>
2025-05-26 20:49:48 +0200

    Footer - Info y diseño responsive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,10 +5,10 @@ export function Footer() {
   return (
     <footer className="footer">
       <div className="footer-contenedor">
-        <p>Centro de Estudios</p>
-        <p> <img src='https://www.systemuicons.com/images/icons/location.svg' /> Dirección: Calle Colegio,  Nº 123, Ciudad Ejemplo</p>
+        <p><strong>IES Centro de Estudios</strong></p>
+        <p> <img src='https://www.systemuicons.com/images/icons/location.svg' /> Dirección: C/Colegio, Nº 123, Ciudad Escolar </p>
         <p> <img src='https://www.systemuicons.com/images/icons/phone_portrait.svg' /> Teléfono: +34 666 666 666</p>
-        <p> <img src='https://www.systemuicons.com/images/icons/mail.svg' /> Email: contacto@centroestudiosficticio.com</p>
+        <p> <img src='https://www.systemuicons.com/images/icons/mail.svg' /> Email: contacto@centroestudios.com</p>
       </div>
     </footer>
 
