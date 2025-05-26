57fff49aa893bc53be9a868c0f3d6c72e84aae5a
AntonioMontilla1 <antonio.montilla.moscoso@gmail.com>
2025-05-26 18:37:24 +0200

    antonio
    modificación tablón anuncios, footer y menú hamburguesa

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,19 +1,24 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Menu } from './components/Menu';
-//import { Footer } from './components/Footer';
-
+import Footer from './components/Footer';
 
 export function Layout() {
+  const [showMenu, setShowMenu] = useState(true);
+
+  const toggleMenu = () => setShowMenu(prev => !prev);
+
   return (
     <div className='layout'>
-        <Header />
-          <div className='layout-content'>
-            <Menu />
-              <div className='content'>
-                <Outlet />
-              </div>
+      <Header onToggleMenu={toggleMenu} />
+      <div className='layout-content'>
+        <Menu isVisible={showMenu} />
+        <div className='content'>
+          <Outlet />
         </div>
+      </div>
+      <Footer />
     </div>
   );
-};
+}
