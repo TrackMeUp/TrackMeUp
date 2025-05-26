fb88073db6a5a2c576e553aa1ab8d1d52c71ad7d
Elena <elenamzuma@gmail.com>
2025-05-26 23:32:46 +0200

    Eliminación de fichero duplicado

--- frontend/src/pages/announcements.jsx
+++ frontend/src/pages/announcements.jsx
@@ -1,7 +1,9 @@
+// Página Tablón de anuncios
+
 export function Announcements() {
     return (
       <div>
-        <h1>Bienvenido a la página de anuncios</h1>
+        <h1>Bienvenido al Tablón de anuncios</h1>
         <p>Este es el contenido de la página de anuncios.</p>
       </div>
     );
